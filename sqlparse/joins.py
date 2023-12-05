7350b2542f4f87670db3f89b45bc95bca3b959c9
Dylan Scott <dscott@hex.tech>
2023-12-04 21:06:20 -0800

    enumerate types

--- sqlparse/joins.py
+++ sqlparse/joins.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 JOIN_TYPES = (
     'CROSS',
     'POSITIONAL',
@@ -6,6 +9,22 @@ JOIN_TYPES = (
         ('', 'INNER', [('LEFT', 'RIGHT', 'FULL'), ('', 'OUTER')]),
     ],
 )
+
+
+def enumerate_types(join_types=[JOIN_TYPES]):
+    if isinstance(join_types, str):
+        yield (join_types,) if join_types else ()
+    elif isinstance(join_types, tuple):
+        for type in join_types:
+            yield from enumerate_types(type)
+    else:
+        combinations = itertools.product(*[
+            enumerate_types(type) for type in join_types
+        ])
+        yield from sorted({
+            tuple(itertools.chain.from_iterable(combo))
+            for combo in combinations
+        })
 
 
 def types_as_regex(join_types=JOIN_TYPES):
